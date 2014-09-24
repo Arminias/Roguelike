fd39d4cb54f42f15507de61e570bdfc09cfd3b1f
Teichimperator <Teichimperator@web.de>
2014-09-24 19:11:11 +0200

    Lieber Arminias, dies ist ein gutgemienter Vorschlag dein Programm zu verbessern!!!!!

--- Roguelike/startercode.py
+++ Roguelike/startercode.py
@@ -11,13 +11,9 @@ class Starter(PygameHelper):
         self.w, self.h = 800, 600
         PygameHelper.__init__(self, size=(self.w, self.h), fill=(255, 255, 255))
 
-        self.SpielerVec = vec2d(20, 20)                           #X Koordinate des Spielers (von links nach rechts)
-                                                                                        #self.SpielerY = 20                                     #Y Koordinate des Spielers (von oben nach unten)
+        self.SpielerVec = vec2d(20, 20)                           #X Koordinate des Spielers (von links nach rechts)self.SpielerY = 20 Y Koordinate des Spielers (von oben nach unten)
         self.pos = vec2d(20,20)
         self.target = vec2d(20,20)
-        self.maximallebenspieler = 100                                  #Maximalleben
-        self.lebenspieler = 100                                    #Aktuelles Leben
-
         self.drawcolor = (0, 0, 0)
         def placeWall (self, koordinateX, koordinateY):                   #Generiert random Zahlen um die Map Random zu Generieren
             randomnumber = random.randint(0,2)
@@ -42,34 +38,33 @@ class Starter(PygameHelper):
             for temp2 in range (1,29):
                 placeWall (self,temp1*20,temp2*20)
 
+        self.maximallebenspieler = 100                                  #Maximalleben
+        self.lebenspieler = 100                                    #Aktuelles Leben
+        self.ErstschlagSpieler = 10
+        self.StaerkeSpieler = 10
+        self.GeschicklichkeitSpieler = 10
+        self.AusdauerSpieler = 10
+        self.MagieSpieler = 10
 
+    def keyDown(self,key):
+            if key == K_w:
+                 self.SpielerVec +=  (0,-20)
+            elif key == K_a:
+                self.SpielerVec += (-20,0)
+            elif key == K_s:
+                self.SpielerVec += (0,20)
+            elif key == K_d:
+                self.SpielerVec += (20,0)
     def update(self):
 
         self.screen.blit(self.img1, (self.SpielerVec))#, self.SpielerY))
         if self.lebenspieler < self.maximallebenspieler:
             self.lebenspieler += 0.0001
         elif self.lebenspieler > self.maximallebenspieler:
-            self.lebenspieler = self.maximallebenspieler
-
-
+            self.lebenspieler = self.maximallebenspieler1
         pass
 
-    def keyUp(self, key):
 
-        #
-       # for event in pygame.event.get():
-       #     if event.type == pygame.KEYDOWN:
-        #     if pygame.event.key == pygame.K_W:
-        if key == 65288:
-                self.SpielerVec =  self.SpielerVec + (20, 0)
-
-
-           # elif pygame.event.key == pygame.K_A:
-            #    self.SpielerY -= 20
-           # elif pygame.event.key == pygame.K_S:
-            #    self.SpielerX += 20
-            #elif pygame.event.key == pygame.K_D:
-             #   self.SpielerY += 20
 
 
 
@@ -86,6 +81,8 @@ class Starter(PygameHelper):
             pygame.event.post(pygame.event.Event(pygame.QUIT))
 
     def draw(self):
+        self.screen.blit(self.img1, (self.SpielerVec))
+        pygame.display.flip()
         pass
 
 
